fa1ca6d17ddeddc6da0049ceb0be0efb40682c07
Sairaj <sairajt@unfyd.com>
2025-11-01 12:01:24 +0530

    add website file

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("greetBtn").addEventListener("click", () => {
+  const msg = document.getElementById("message");
+  msg.textContent = "Hello Sairaj! 👋 Thanks for visiting my website!";
+  msg.style.fontWeight = "bold";
+});
